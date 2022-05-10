91d111358fdbbc3cab69a26ecc87e48bd1649b98
GHANAM Aymane <aymaneghanam123@gmail.com>
2022-05-10 11:14:29 +0200

    Classe Utilisateur

---
+++ Model/Utilisateur.cpp
@@ -0,0 +1,47 @@
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Utilisateur.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+Utilisateur::Utilisateur(string login, string mdp)
+{
+    this->identifiantUtilisateur = login;
+    this->motDePasse = mdp;
+#ifdef MAP
+    cout << "Appel au constructeur de <Xxx>" << endl;
+#endif
+}
+
+string Utilisateur::getIdentifiantUtilisateur() const
+{
+    return this->identifiantUtilisateur;
+}
+
+string Utilisateur::getMotDePasse() const
+{
+    return this->motDePasse;
+}
+
+Utilisateur::~Utilisateur()
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Xxx>" << endl;
+#endif
+} //----- Fin de ~Xxx
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
---
+++ Model/Utilisateur.h
@@ -0,0 +1,41 @@
+#if !defined(UTILISATEUR_H)
+#define UTULISATEUR_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+using namespace std;
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+class Utilisateur
+{
+    //----------------------------------------------------------------- PUBLIC
+
+public:
+    //----------------------------------------------------- Méthodes publiques
+
+    //------------------------------------------------- Surcharge d'opérateurs
+
+    //-------------------------------------------- Constructeurs - destructeur
+    Utilisateur(string login, string mdp);
+
+    string getIdentifiantUtilisateur() const;
+
+    string getMotDePasse() const;
+
+    virtual ~Utilisateur();
+
+    //------------------------------------------------------------------ PRIVE
+
+protected:
+    string identifiantUtilisateur;
+    string motDePasse;
+    //----------------------------------------------------- Méthodes protégées
+
+    //----------------------------------------------------- Attributs protégés
+};
+
+//-------------------------------- Autres définitions dépendantes de <Xxx>
+
+#endif // UTILISATEUR_H
