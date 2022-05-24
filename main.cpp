8c66fb39e4d50a4761b4ce6bd4d01440fdd06e9e
Flandre Corenitn <flandrecorentin@gmail.com>
2022-05-24 11:19:38 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+#include <fstream>
+#include <istream>
+#include <list>
+#include "./Model/Temps.h"
+#include "./Model/Capteur.h"
+
+
+
+
+
+
+static list<Capteur> maListeDeCapteur; 
+void initialisationCapteur(){
+    string id; 
+    float lat;
+    float lon; 
+    
+    ifstream fic;
+    
+    fic.open("./dataset/sensors.csv");
+    if(!fic){
+        cout << " *****  LE FICHIER EST INTROUVABLE" << endl;
+    }
+
+    // list<Capteur>::iterator it = maListeDeCapteur.begin();
+    // char * buffer = new char[100];
+    string buffer;
+     getline(fic, buffer, ';');
+    while(!fic.eof()){
+        
+        id = buffer; 
+        cout <<"BUFFER id: " <<buffer << endl;
+        getline(fic, buffer, ';');
+        cout <<"BUFFER lat: " <<buffer << endl;
+        lat = stof(buffer);
+        getline(fic, buffer, ';');
+        cout <<"BUFFER lon: " <<buffer << endl;
+        lon = stof(buffer);
+        getline(fic, buffer, ';');
+        // cout <<"FIN: " <<buffer << endl;
+        Capteur capteurARajoute = Capteur(id,lat,lon);
+        maListeDeCapteur.push_back(capteurARajoute);
+        // it++;
+    }
+}
+
+
+
+
+
+
+
+
+
+void initialiserAirWatcher(){
+    cout << "** debut initialisation capteurs " << endl;
+    initialisationCapteur();
+    cout << "** fin initialisation capteurs " << endl;
+
+    // cout << "** debut initialisation capteurs " << endl;
+    // initialisationCapteur();
+    // cout << "** fin initialisation capteurs " << endl;
+}
+
+int main(){
+    cout << "********* DEBUT INITIALISATION" << endl;
+    initialiserAirWatcher();
+    cout << "********* FIN INITIALISATION" << endl;
+    return 0;
+}
