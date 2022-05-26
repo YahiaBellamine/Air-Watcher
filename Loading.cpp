d30d404f3384820ecde50127cbc2e4ca74eb8dc3
Flandre Corenitn <flandrecorentin@gmail.com>
2022-05-26 22:34:28 +0200

    commit loading.cpp principalement

--- Loading.cpp
+++ Loading.cpp
@@ -13,6 +13,7 @@ using namespace std;
 #include "Model/Capteur.h"
 #include "Model/Fournisseur.h"
 #include "Model/IndividuPrive.h"
+#include "Controller/ActionNettoyeur.h"
 
 static list<SerieMesures> listeSeriesMesures;
 static list<Attribut> listeAttributs;
@@ -215,5 +216,207 @@ int main(){
     cout << "****** Debut test initialisation des utilisateurs" << endl;
     loadUsers();
     cout << "****** Fin test initialisation des utilisateurs" << endl;
+
+
+    cout << "Veuillez choisir le type de compte auquel vous voulez vous connecter :" << endl;
+    cout << "   1. Agence gouvernementale"<< endl;
+    cout << "   2. Utilisateur privé"<< endl;
+    cout << "   3. Fournisseur"<< endl;
+    cout << "" << endl;
+    cout << "   0. Se déconnecter"<< endl;
+
+    string identifiant;
+    int buffer; 
+    string type;
+    scanf("%d", &buffer);
+    bool etape1=true;
+    while(etape1){
+        if(buffer==1){
+            type = "agenceGouvernementale";
+            etape1 =false;
+        }
+        else if(buffer==2){
+            type = "individuPrive";
+
+            etape1 =false;
+        }
+        else if(buffer==3){
+            type = "fournisseur";
+            etape1 =false;
+        }
+        else if(buffer ==0){
+            return 0;
+        }
+        else{
+            printf("/!\\ Requête non reconnue : Sécurité/Comportement non prévu -> quitter l'application\r\n");
+            // scanf("%d", &buffer);
+            return 0;
+        }
+    }
+
+
+
+    cout << "Veuillez entrer l'identifiant de votre compte" << endl;
+    if(type=="agenceGouvernementale"){
+        cout << "identifiant: 7" << endl;
+        scanf("%d", &buffer);
+        if(buffer==7) {
+            identifiant = "agence";
+            }
+        else{
+            printf("/!\\ Identifiant introuvable\r\n");
+            return 0;
+        }
+    }
+    else if(type=="individuPrive"){
+        cout << "identifiant 0 ou 1" << endl;
+        scanf("%d", &buffer);
+        if(buffer==0) {
+            identifiant = "User0";
+            // valeur de point arbitraire
+            // listeUtilisateurs[identifiant].setNombrePoints(12);
+        }
+        else if(buffer==1) {
+            identifiant = "User1";
+            // valeur de point arbitraire
+            // listeUtilisateurs[identifiant].setNombrePoints(25);
+            }
+        else{
+            printf("/!\\ Identifiant introuvable\r\n");
+            return 0;
+        }
+    }
+    else if(type=="fournisseur"){
+        cout << "identifiant 0 ou 1" << endl;
+        scanf("%d", &buffer);
+        if(buffer==0) {
+            identifiant = "Provider0";
+            // valeur de point arbitraire
+        }
+        else if(buffer==1) {
+            identifiant = "Provider1";
+            // valeur de point arbitraire
+            }
+        else{
+            printf("/!\\ Identifiant introuvable\r\n");
+            return 0;
+        }
+    }
+    
+
+    bool etape2 = true;
+    while(etape2){
+        if(type=="agenceGouvernementale"){
+            type = "agenceGouvernementale";
+            cout << "Veuillez choisir parmi les fonctionnalités suivantes :" << endl;
+            cout << "" << endl;
+            cout << "   1. Demander la moyenne de la qualité de l'air dans une zone géographique, en moyenne sur une période ou en instantanée" << endl;
+            cout << "   2. Demander la qualité de l'air à un point précis de la carte, contenant ou non un capteur" << endl;
+            cout << "   3. Comparer les similarités entre capteurs" << endl;
+            cout << "   4. Verifier la fiabilité des capteurs et des individus privés et les exclure selon la fiabilité" << endl;
+            cout << "" << endl;
+            cout << "   0. Se déconnecter"<< endl;
+            scanf("\r\n%d", &buffer);
+        }
+        else if(type=="individuPrive"){
+            type = "individuPrive";
+            cout << "Veuillez choisir parmi les fonctionnalités suivantes :" << endl;
+            cout << "" << endl;
+            cout << "   1. Connaitre le nombre de points" << endl;
+            cout << "   2. Connaitre ses données de capteur personnel" << endl;
+            cout << "" << endl;
+            cout << "   0. Se déconnecter"<< endl;
+            scanf("\r\n%d", &buffer);
+        }
+        else if(type=="fournisseur"){
+            type = "fournisseur";
+            cout << "Veuillez choisir parmi les fonctionnalités suivantes :" << endl;
+            cout << "" << endl;
+            cout << "   1. Observer l'impact des purificateurs d'air sur la qualité de l'air" << endl;
+            cout << "" << endl;
+            cout << "   0. Se déconnecter"<< endl;
+            scanf("\r\n%d", &buffer);
+        }
+        else if(buffer ==0){
+            return 0;
+        }
+        else{
+            printf("/!\\ Requête non reconnue : Sécurité/Comportement non prévu -> quitter l'application\r\n");
+            // scanf("%d", &buffer);
+            return 0;
+        }
+
+
+
+        if (buffer==1){
+            if(type=="agenceGouvernementale"){
+
+            }
+            else if(type=="individuPrive"){
+                if(identifiant=="User0"){
+                    // cout << "\nNombre de points : "<< listeUtilisateurs["User0"].getNombrePoints() <<"\n"<< endl;
+                }
+                else if(identifiant=="User1"){
+                    // cout << "\nNombre de points : "<< listeUtilisateurs["User1"].getNombrePoints() <<"\n"<< endl;
+                }
+                else{
+                    cout << "\nNombre de points : "<< 0 <<"\n"<< endl;
+                }
+            }
+            else if(type=="fournisseur"){
+                // evaluerImpactNettoyeur(listeNettoyeurs.get(0), 0, 0, 100);
+            }
+        }
+        else if (buffer==2){
+            if(type=="agenceGouvernementale"){
+
+            }
+            else if(type=="individuPrive"){
+
+            }
+            else if(type=="fournisseur"){
+                printf("/!\\ Requête non reconnue : Sécurité/Comportement non prévu -> quitter l'application\r\n");
+                return 0;
+            }
+            
+        }
+        else if (buffer==3){
+            if(type=="agenceGouvernementale"){
+                //vector<Capteur> resultat = comparerCapteur(Capteur &capteurSelectionne, vector<Capteur> &listeCapteurs)
+            }
+            else if(type=="individuPrive"){
+                printf("/!\\ Requête non reconnue : Sécurité/Comportement non prévu -> quitter l'application\r\n");
+                return 0;
+            }
+            else if(type=="fournisseur"){
+                printf("/!\\ Requête non reconnue : Sécurité/Comportement non prévu -> quitter l'application\r\n");
+                return 0;
+            }
+        }
+        else if (buffer==4){
+            if(type=="agenceGouvernementale"){
+
+            }
+            else if(type=="individuPrive"){
+                printf("/!\\ Requête non reconnue : Sécurité/Comportement non prévu -> quitter l'application\r\n");
+                return 0;
+            }
+            else if(type=="fournisseur"){
+                printf("/!\\ Requête non reconnue : Sécurité/Comportement non prévu -> quitter l'application\r\n");
+                return 0;
+            }
+        }
+        else if(buffer ==0){
+            return 0;
+        }
+        else{
+            printf("/!\\ Requête non reconnue : Sécurité/Comportement non prévu -> quitter l'application\r\n");
+            // scanf("%d", &buffer);
+            return 0;
+        }
+
+
+    }
+    
     return 0;
 }
--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ else
 	CXXFLAGS=-ansi -pedantic -Wall -std=c++17 -g
 endif
 EXEC=main
-OBJECTS=mainAlexis.cpp Model/Capteur.o Model/SerieMesures.o Model/Mesure.o Controller/ActionCapteur.o Controller/ActionQualiteAir.o Model/AgenceGouvernementale.o Model/Attribut.o Model/Fournisseur.o Model/IndividuPrive.o Model/Nettoyeur.o Model/Utilisateur.o Controller/ServiceUtilisateur.o Controller/ActionNettoyeur.o
-TEST = mainAlexis.cpp Model/Capteur.o Model/SerieMesures.o Model/Mesure.o Controller/ActionCapteur.o
+OBJECTS= Model/Capteur.o Model/SerieMesures.o Model/Mesure.o Controller/ActionCapteur.o Controller/ActionQualiteAir.o Model/AgenceGouvernementale.o Model/Attribut.o Model/Fournisseur.o Model/IndividuPrive.o Model/Nettoyeur.o Model/Utilisateur.o Controller/ServiceUtilisateur.o Controller/ActionNettoyeur.o
+TEST =  Model/Capteur.o Model/SerieMesures.o Model/Mesure.o Controller/ActionCapteur.o
 
 all: $(OBJECTS)
 	$(CXX) -o $(EXEC) $(OBJECTS)
